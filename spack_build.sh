fb9ec3ed5af1b204dfbda1ecd813e461efdbe78f
Andreas Stefl <stefl.andreas@gmail.com>
2025-10-22 07:24:35 +0200

    revert some stuff

--- spack_build.sh
+++ spack_build.sh
@@ -45,7 +45,8 @@ end_section
 echo "Spack version: $(spack --version)"
 
 start_section "Create environment"
-spack env create -d . "$SCRIPT_DIR"/spack.yaml
+spack env create -d . "$SCRIPT_DIR"/spack.yaml 
+# spack -e . mirror set --autopush acts-spack-buildcache
 end_section
 
 start_section "List visible compilers"
